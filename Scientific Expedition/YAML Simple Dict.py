007440f7e0434349ec213d81433fa510ff6078c1
Bundi-py <58522757+Bundi-py@users.noreply.github.com>
2020-05-26 21:40:29 +0630

    commit 3

--- Scientific Expedition/YAML Simple Dict.py
+++ Scientific Expedition/YAML Simple Dict.py
@@ -1,15 +1,64 @@
+#  Have you ever heard of such markup language as YAML? It’s a friendly data serialization format.
+# In fact it’s so friendly that both people and programs can read it quite well. You can play
+# around with the standard by following this link.
+
+# YAML is a text, and you need to convert it into an object. But I’m not asking you to implement
+# the entire YAML standard, we’ll implement it step by step.
+
+# The first step is the key-value conversion. The key can be any string consisting of Latin letters
+# and numbers. The value can be a single-line string (which consists of spaces, Latin letters and
+# numbers) or a number (int).
+
+# I’ll show some examples:
+
+# name: Alex
+# age: 12
+
+# Converted into an object.
+
+# {
+#   "name": "Alex",
+#   "age": 12
+# }
+
+# Note that the number automatically gets type int
+# Another example shows that the string may contain spaces.
+
+# name: Alex Fox
+# age: 12
+
+# class: 12b
+
+# Will be converted into the next object.
+
+# {
+#   "age": 12,
+#   "name": "Alex Fox",
+#   "class": "12b"
+# }
+
+# Pay attention to a few things. Between the string "age" and the string "class" there is an empty
+# string that doesn’t interfere with parsing. The class starts with numbers, but has letters, which
+# means it cannot be converted to numbers, so its type remains a string (str).
+
+# Input: A format string.
+# Output: An object.
+
+
 def yaml(a):
     recnik = {}
     b = []
     a = a.split('\n')
-    a = [i for i in a if len(i) != 0]
     a = sorted(a)
     for i in a:
-        b = i.split(':')
-        recnik[b[0]] = b[1].strip()
-    for k in recnik:
-        if k == 'age':
-            recnik[k] = int(recnik[k])
+        if len(i) != 0:
+            b = i.split(':')
+            recnik[b[0]] = b[1].strip()
+    for k, v in recnik.items():
+        if v.isdigit():
+            recnik[k] = int(v)
+        else:
+            recnik[k] = v
     return recnik
 
 
